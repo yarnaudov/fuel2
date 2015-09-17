458683126ceff9c409191556dc2955c19bb0b1d6
Yordan Arnaudov <yarnaudov@vayant.com>
2015-09-17 14:53:07 +0300

    add crpt.php config file

---
+++ fuel/app/config/crpt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package     Fuel
+ * @version     1.7
+ * @author      Fuel Development Team
+ * @license     MIT License
+ * @copyright   2010 - 2015 Fuel Development Team
+ * @link        http://fuelphp.com
+ */
+
+return array (
+	'crypto_key' => 'vkMFv4DKwoIsAjMOu8NDg23o',
+	'crypto_iv' => 'Nz0hqs-r8ClAGxEWJI3EATEI',
+	'crypto_hmac' => '9-QtDoSY85osWhA2OYWNsDEg',
+);
